0435ae58d0c6782103c20b93c4d5bfd0e46c4abd
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-04-15 01:19:12 +0800

    Fix a bug when using offline script with BUILD_XXX=3.

--- dtk/build_offline.sh
+++ dtk/build_offline.sh
@@ -34,17 +34,26 @@ function build() {
     fi
 }
 
-function prepare_quda() {
+function build_quda() {
     if [ $1 -gt 0 ]; then
-        mkdir -p ${BIN}/$2
-        pushd ${BIN}/$2
-        if !([ -f cmake/CPM_0.38.5.cmake ] && [ "$(sha256sum cmake/CPM_0.38.5.cmake | awk '{print $1}')" == "192aa0ccdc57dfe75bd9e4b176bf7fb5692fd2b3e3f7b09c74856fc39572b31c" ]); then
-            mkdir -p cmake
-            cp ${ROOT}/CPM_0.38.5.cmake cmake
-        fi
-        if !([ -f _deps/eigen-subbuild/eigen-populate-prefix/src/eigen-3.4.0.tar.bz2 ] && [ "$(sha256sum _deps/eigen-subbuild/eigen-populate-prefix/src/eigen-3.4.0.tar.bz2 | awk '{print $1}')" == "b4c198460eba6f28d34894e3a5710998818515104d6e74e5cc331ce31e46e626" ]); then
-            mkdir -p _deps/eigen-subbuild/eigen-populate-prefix/src
-            cp ${ROOT}/eigen-3.4.0.tar.bz2 _deps/eigen-subbuild/eigen-populate-prefix/src
+        mkdir -p ${BIN}/$3
+        pushd ${BIN}/$3
+        if [ $1 -gt 1 ]; then
+            rm -rf CMakeCache.txt
+            if [ $1 -gt 2 ]; then
+                rm -rf ./*
+            fi
+            if !([ -f cmake/CPM_0.38.5.cmake ] && [ "$(sha256sum cmake/CPM_0.38.5.cmake | awk '{print $1}')" == "192aa0ccdc57dfe75bd9e4b176bf7fb5692fd2b3e3f7b09c74856fc39572b31c" ]); then
+                mkdir -p cmake
+                cp ${ROOT}/CPM_0.38.5.cmake cmake
+            fi
+            if !([ -f _deps/eigen-subbuild/eigen-populate-prefix/src/eigen-3.4.0.tar.bz2 ] && [ "$(sha256sum _deps/eigen-subbuild/eigen-populate-prefix/src/eigen-3.4.0.tar.bz2 | awk '{print $1}')" == "b4c198460eba6f28d34894e3a5710998818515104d6e74e5cc331ce31e46e626" ]); then
+                mkdir -p _deps/eigen-subbuild/eigen-populate-prefix/src
+                cp ${ROOT}/eigen-3.4.0.tar.bz2 _deps/eigen-subbuild/eigen-populate-prefix/src
+            fi
+            ${@:4} && cmake --build . -j$2 && cmake --install .
+        else
+            cmake --build . -j$2 && cmake --install .
         fi
         popd
     fi
@@ -101,8 +110,7 @@ build ${BUILD_QDP_JIT} ${JOBS} qdp-jit \
     -DCMAKE_INSTALL_RPATH=${DST}/qdp-jit/lib -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=True \
     -DCMAKE_INSTALL_PREFIX=${DST}/qdp-jit ${SRC}/qdp-jit
 
-prepare_quda ${BUILD_PYQUDA} quda
-build ${BUILD_QUDA} ${QUDA_JOBS} quda \
+build_quda ${BUILD_QUDA} ${QUDA_JOBS} quda \
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=RELEASE -DQUDA_BUILD_SHAREDLIB=${BUILD_SHAREDLIB} \
     -DQUDA_TARGET_TYPE=HIP -DAMDGPU_TARGETS=${GPU_TARGET} -DGPU_TARGETS=${GPU_TARGET} \
     -DQUDA_GPU_ARCH=${GPU_TARGET} -DQUDA_HETEROGENEOUS_ATOMIC=${HETEROGENEOUS_ATOMIC} \
@@ -111,8 +119,7 @@ build ${BUILD_QUDA} ${QUDA_JOBS} quda \
     -DQMP_DIR=${DST}/qmp/lib/cmake/QMP -DQIO_DIR=${DST}/qdpxx/lib/cmake/QIO \
     -DCMAKE_INSTALL_PREFIX=${DST}/quda ${SRC}/quda
 
-prepare_quda ${BUILD_PYQUDA} quda-jit
-build ${BUILD_QUDA_JIT} ${QUDA_JOBS} quda-jit \
+build_quda ${BUILD_QUDA_JIT} ${QUDA_JOBS} quda-jit \
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=RELEASE -DQUDA_BUILD_SHAREDLIB=${BUILD_SHAREDLIB} \
     -DQUDA_TARGET_TYPE=HIP -DAMDGPU_TARGETS=${GPU_TARGET} -DGPU_TARGETS=${GPU_TARGET} \
     -DQUDA_GPU_ARCH=${GPU_TARGET} -DQUDA_HETEROGENEOUS_ATOMIC=${HETEROGENEOUS_ATOMIC} \
@@ -122,8 +129,7 @@ build ${BUILD_QUDA_JIT} ${QUDA_JOBS} quda-jit \
     -DQMP_DIR=${DST}/qmp/lib/cmake/QMP -DQIO_DIR=${DST}/qdpxx/lib/cmake/QIO -DQDPXX_DIR=${DST}/qdp-jit/lib/cmake/QDPXX \
     -DCMAKE_INSTALL_PREFIX=${DST}/quda-jit ${SRC}/quda
 
-prepare_quda ${BUILD_PYQUDA} pyquda
-build ${BUILD_PYQUDA} ${QUDA_JOBS} pyquda \
+build_quda ${BUILD_PYQUDA} ${QUDA_JOBS} pyquda \
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=RELEASE -DQUDA_BUILD_SHAREDLIB=ON \
     -DQUDA_TARGET_TYPE=HIP -DAMDGPU_TARGETS=${GPU_TARGET} -DGPU_TARGETS=${GPU_TARGET} \
     -DQUDA_GPU_ARCH=${GPU_TARGET} -DQUDA_HETEROGENEOUS_ATOMIC=${HETEROGENEOUS_ATOMIC} \
