b359e83e752ee38f8a7a770e046da20a2b25c9fd
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-05-07 16:32:34 +0800

    The latest commit of QUDA breaks HMC.

--- download.sh
+++ download.sh
@@ -49,7 +49,7 @@ if !([ ${TARGET} == "cpu" ]); then
     pushd quda
     git checkout develop
     git pull
-    git checkout 56ab21cbb
+    git checkout c051eac9b
     popd
 
     if !([ -f CPM_0.38.5.cmake ] && [ "$(sha256sum CPM_0.38.5.cmake | awk '{print $1}')" == "192aa0ccdc57dfe75bd9e4b176bf7fb5692fd2b3e3f7b09c74856fc39572b31c" ]); then
