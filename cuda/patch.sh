6b55846177228c1dfb4f626912353db12c17c55f
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-07-03 16:38:15 +0800

    Add patch for Debian/Ubuntu.

--- cuda/patch.sh
+++ cuda/patch.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# if [ -d ${SCIDAC}/qdp-jit ]; then
+#     pushd ${SCIDAC}/qdp-jit
+#     git restore *
+#     git apply ${DIR}/patch/qdp-jit_debian.patch
+#     popd
+# fi
+
 if [ -d ${SCIDAC}/quda ]; then
     pushd ${SCIDAC}/quda
     git restore *
