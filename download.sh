cfb93009f86a670f23c9e965d37ef09e695f8bd0
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-12-26 17:52:17 +0800

    Update quda commit.

--- download.sh
+++ download.sh
@@ -54,7 +54,7 @@ if !([ ${TARGET} == "cpu" ]); then
     git restore *
     git checkout develop
     git pull
-    git checkout 3ce35dcd2
+    git checkout b58f1ecb5
     popd
 
     if !([ -f CPM_0.40.2.cmake ] && [ "$(sha256sum CPM_0.40.2.cmake | awk '{print $1}')" == "c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d" ]); then
