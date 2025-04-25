ecc88245c1cd585b4d684c5a815f44062d64cdad
SaltyChiang <SaltyChiang@users.noreply.github.com>
2025-04-25 23:57:41 +0800

    Patch QDP-JIT for explicitly linking against LLVM libraries.

--- cuda/patch.sh
+++ cuda/patch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# if [ -d ${SCIDAC}/qdp-jit ]; then
-#     pushd ${SCIDAC}/qdp-jit
-#     git restore *
-#     git apply ${DIR}/patch/qdp-jit_debian.patch
-#     popd
-# fi
+if [ -d ${SCIDAC}/qdp-jit ]; then
+    pushd ${SCIDAC}/qdp-jit
+    git restore *
+    git apply ${DIR}/patch/qdp-jit_debian.patch
+    popd
+fi
--- dtk/patch.sh
+++ dtk/patch.sh
@@ -3,6 +3,7 @@
 if [ -d ${SCIDAC}/qdp-jit ]; then
     pushd ${SCIDAC}/qdp-jit
     git restore *
+    git apply ${DIR}/patch/qdp-jit_debian.patch
     git apply ${DIR}/patch/qdp-jit_dtk.patch
     popd
 fi
