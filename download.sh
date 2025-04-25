ab5d135361234f6ba7a2d30d9e64d50629586c71
SaltyChiang <SaltyChiang@users.noreply.github.com>
2025-04-25 19:19:03 +0800

    Update Chroma and QUDA commit.

--- download.sh
+++ download.sh
@@ -35,7 +35,7 @@ pushd chroma
 git restore *
 git checkout devel
 git pull
-git checkout 9e78369de
+git checkout a761e7e03
 git submodule update --recursive
 popd
 
@@ -54,7 +54,7 @@ if !([ ${TARGET} == "cpu" ]); then
     git restore *
     git checkout develop
     git pull
-    git checkout e23cd7e4f
+    git checkout dca4b59a0
     popd
 
     if !([ -f CPM_0.40.2.cmake ] && [ "$(sha256sum CPM_0.40.2.cmake | awk '{print $1}')" == "c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d" ]); then
