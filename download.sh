b5425134aa8fd97ca1872adfa97aeccf6e60fc63
SaltyChiang <SaltyChiang@users.noreply.github.com>
2025-10-03 23:44:56 +0800

    Revert QUDA commit (vectorize).

--- download.sh
+++ download.sh
@@ -68,7 +68,7 @@ if !([ ${TARGET} == "openmp" ]); then
     git checkout .
     git checkout develop
     git pull
-    git checkout 08c6c016b
+    git checkout 07822b61c
     popd
 
     if !([ -f CPM_0.40.2.cmake ] && [ "$(sha256sum CPM_0.40.2.cmake | awk '{print $1}')" == "c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d" ]); then
