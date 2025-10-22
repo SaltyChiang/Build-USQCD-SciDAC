1f4bb6cb39ec77f8832b2122e5afa25df25a80fa
SaltyChiang <SaltyChiang@users.noreply.github.com>
2025-10-22 08:59:01 +0800

    Format fix.

--- pyquda.sh
+++ pyquda.sh
@@ -23,6 +23,7 @@ git_checkout quda \
 wget_sha256 CPM_0.40.2.cmake \
     https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake \
     c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d
+
 wget_sha256 e67c494cba7180066e73b9f6234d0b2129f1cdf5.tar.bz2 \
     https://gitlab.com/libeigen/eigen/-/archive/e67c494cba7180066e73b9f6234d0b2129f1cdf5.tar.bz2 \
     98d244932291506b75c4ae7459af29b1112ea3d2f04660686a925d9ef6634583
